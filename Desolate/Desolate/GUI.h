4578f9a364a4b81910be8bcbe4bcc9017f8d664f
conixtract <conixtract@gmail.com>
2019-03-07 19:27:37 +0100

    coderefactoring and added images: -added GUIThirst2.png to Data -added GUIHunger2.png to Data -refactored GUI.h -refactored MainGUI.h

--- Desolate/Desolate/GUI.h
+++ Desolate/Desolate/GUI.h
@@ -7,15 +7,15 @@
 
 class GUI {
 public:
-	GUI(sf::RenderWindow* window, TextureManager* tm) {
-		mWindow = window;
-		mTextureManager = tm;
+	GUI(sf::RenderWindow* window, TextureManager* tM) {
+		window_ = window;
+		tM_ = tM;
 		std::cout << "GUI Initialized" << std::endl;
 	}
 
 	virtual ~GUI() {
-		for (int i = 0; i < mGUIElements.size(); ++i) {
-			delete mGUIElements[i];
+		for (int i = 0; i < GUI_elements_.size(); ++i) {
+			delete GUI_elements_[i];
 		}
 	}
 
@@ -23,20 +23,20 @@ public:
 
 	}
 
-	virtual void update(float deltaTime) {
+	virtual void update(float dT) {
 
 	}
 
 	virtual void render() {
-		for (int i = 0; i < mGUIElements.size(); ++i) {
-			mWindow->draw(*mGUIElements[i]);
+		for (int i = 0; i < GUI_elements_.size(); ++i) {
+			window_->draw(*GUI_elements_[i]);
 		}
 	}
 protected:
-	sf::RenderWindow* mWindow;
-	TextureManager* mTextureManager;
+	sf::RenderWindow* window_;
+	TextureManager* tM_;
 
-	std::vector<sf::Drawable*> mGUIElements;
+	std::vector<sf::Drawable*> GUI_elements_;
 };
 
 #endif
--- Desolate/Desolate/MainGUI.h
+++ Desolate/Desolate/MainGUI.h
@@ -11,206 +11,206 @@
 
 class MainGUI : public GUI {
 public:
-	MainGUI(sf::RenderWindow* window, TextureManager* tm, Player* player) : GUI(window, tm) {
-		mPlayer = player;
-		mBoxSelected = 0;
+	MainGUI(sf::RenderWindow* window, TextureManager* tM, Player* player) : GUI(window, tM) {
+		player_ = player;
+		box_selected_ = 0;
 
-		mHealthy = sf::Color(13, 142, 8);
-		mWarning = sf::Color(214, 198, 55);
-		mDanger = sf::Color(160, 23, 11);
-		mFatal = sf::Color(0, 0, 0);
+		healthy_ = sf::Color(13, 142, 8);
+		warning_ = sf::Color(214, 198, 55);
+		danger_ = sf::Color(160, 23, 11);
+		fatal_ = sf::Color(0, 0, 0);
 
-		mHungerMeter = new sf::Sprite(mTextureManager->getTexture("GUIHunger"));
-		mHungerMeter->setScale(sf::Vector2f(2.0f, 2.0f));
-		mHungerMeter->setPosition(sf::Vector2f(mPlayer->getPosition().x + 310, mPlayer->getPosition().y - 445));
+		hunger_meter_ = new sf::Sprite(tM_->getTexture("GUIHunger"));
+		hunger_meter_->setScale(sf::Vector2f(2.0f, 2.0f));
+		hunger_meter_->setPosition(sf::Vector2f(player_->getPosition().x + 310, player_->getPosition().y - 445));
 
-		mThirstMeter = new sf::Sprite(mTextureManager->getTexture("GUIThirst"));
-		mThirstMeter->setScale(sf::Vector2f(2.0f, 2.0f));
-		mThirstMeter->setPosition(sf::Vector2f(mPlayer->getPosition().x + 385, mPlayer->getPosition().y - 445));
+		thrist_meter_ = new sf::Sprite(tM_->getTexture("GUIThirst"));
+		thrist_meter_->setScale(sf::Vector2f(2.0f, 2.0f));
+		thrist_meter_->setPosition(sf::Vector2f(player_->getPosition().x + 385, player_->getPosition().y - 445));
 
-		mHealthMeter = new sf::Sprite(mTextureManager->getTexture("GUIHealth"));
-		mHealthMeter->setScale(sf::Vector2f(2.0f, 2.0f));
-		mHealthMeter->setPosition(sf::Vector2f(mPlayer->getPosition().x + 460, mPlayer->getPosition().y - 445));
+		health_meter_ = new sf::Sprite(tM_->getTexture("GUIHealth"));
+		health_meter_->setScale(sf::Vector2f(2.0f, 2.0f));
+		health_meter_->setPosition(sf::Vector2f(player_->getPosition().x + 460, player_->getPosition().y - 445));
 
-		mRestMeter = new sf::Sprite(mTextureManager->getTexture("GUIRest"));
-		mRestMeter->setScale(sf::Vector2f(2.0f, 2.0f));
-		mRestMeter->setPosition(sf::Vector2f(mPlayer->getPosition().x + 535, mPlayer->getPosition().y - 445));
+		rest_meter = new sf::Sprite(tM_->getTexture("GUIRest"));
+		rest_meter->setScale(sf::Vector2f(2.0f, 2.0f));
+		rest_meter->setPosition(sf::Vector2f(player_->getPosition().x + 535, player_->getPosition().y - 445));
 
-		mBackBorder = new sf::RectangleShape(sf::Vector2f(560, 120));
-		mBackBorder->setPosition(sf::Vector2f(mPlayer->getPosition().x - 280.0f, mPlayer->getPosition().y + 355));
-		mBackBorder->setFillColor(sf::Color(10, 10, 10, 200));
+		backboarder_ = new sf::RectangleShape(sf::Vector2f(560, 120));
+		backboarder_->setPosition(sf::Vector2f(player_->getPosition().x - 280.0f, player_->getPosition().y + 355));
+		backboarder_->setFillColor(sf::Color(10, 10, 10, 200));
 
-		mHungerLevel = new sf::CircleShape(30.0f);
-		mHungerLevel->setFillColor(mHealthy);
+		hunger_level_ = new sf::CircleShape(30.0f);
+		hunger_level_->setFillColor(healthy_);
 
-		mThirstLevel = new sf::CircleShape(30.0f);
-		mThirstLevel->setFillColor(mHealthy);
+		thirst_level_ = new sf::CircleShape(30.0f);
+		thirst_level_->setFillColor(healthy_);
 
-		mHealthLevel = new sf::CircleShape(30.0f);
-		mHealthLevel->setFillColor(mHealthy);
+		health_level_ = new sf::CircleShape(30.0f);
+		health_level_->setFillColor(healthy_);
 
-		mRestLevel = new sf::CircleShape(30.0f);
-		mRestLevel->setFillColor(mHealthy);
+		rest_level = new sf::CircleShape(30.0f);
+		rest_level->setFillColor(healthy_);
 
-		mBox0 = new sf::RectangleShape(sf::Vector2f(100, 100));
-		mBox0->setPosition(sf::Vector2f(sf::Vector2f(mBackBorder->getPosition().x + 10, mBackBorder->getPosition().y + 10)));
-		mBox0->setFillColor(sf::Color(10, 10, 10, 230));
-		mBox0->setOutlineColor(sf::Color::White);
+		box0_ = new sf::RectangleShape(sf::Vector2f(100, 100));
+		box0_->setPosition(sf::Vector2f(sf::Vector2f(backboarder_->getPosition().x + 10, backboarder_->getPosition().y + 10)));
+		box0_->setFillColor(sf::Color(10, 10, 10, 230));
+		box0_->setOutlineColor(sf::Color::White);
 
-		mBox1 = new sf::RectangleShape(sf::Vector2f(100, 100));
-		mBox1->setPosition(sf::Vector2f(sf::Vector2f(mBackBorder->getPosition().x + 120, mBackBorder->getPosition().y + 10)));
-		mBox1->setFillColor(sf::Color(10, 10, 10, 230));
-		mBox1->setOutlineColor(sf::Color::White);
+		box1_ = new sf::RectangleShape(sf::Vector2f(100, 100));
+		box1_->setPosition(sf::Vector2f(sf::Vector2f(backboarder_->getPosition().x + 120, backboarder_->getPosition().y + 10)));
+		box1_->setFillColor(sf::Color(10, 10, 10, 230));
+		box1_->setOutlineColor(sf::Color::White);
 
-		mBox2 = new sf::RectangleShape(sf::Vector2f(100, 100));
-		mBox2->setPosition(sf::Vector2f(sf::Vector2f(mBackBorder->getPosition().x + 230, mBackBorder->getPosition().y + 10)));
-		mBox2->setFillColor(sf::Color(10, 10, 10, 230));
-		mBox2->setOutlineColor(sf::Color::White);
+		box2_ = new sf::RectangleShape(sf::Vector2f(100, 100));
+		box2_->setPosition(sf::Vector2f(sf::Vector2f(backboarder_->getPosition().x + 230, backboarder_->getPosition().y + 10)));
+		box2_->setFillColor(sf::Color(10, 10, 10, 230));
+		box2_->setOutlineColor(sf::Color::White);
 
-		mBox3 = new sf::RectangleShape(sf::Vector2f(100, 100));
-		mBox3->setPosition(sf::Vector2f(sf::Vector2f(mBackBorder->getPosition().x + 340, mBackBorder->getPosition().y + 10)));
-		mBox3->setFillColor(sf::Color(10, 10, 10, 230));
-		mBox3->setOutlineColor(sf::Color::White);
+		box3_ = new sf::RectangleShape(sf::Vector2f(100, 100));
+		box3_->setPosition(sf::Vector2f(sf::Vector2f(backboarder_->getPosition().x + 340, backboarder_->getPosition().y + 10)));
+		box3_->setFillColor(sf::Color(10, 10, 10, 230));
+		box3_->setOutlineColor(sf::Color::White);
 
-		mBox4 = new sf::RectangleShape(sf::Vector2f(100, 100));
-		mBox4->setPosition(sf::Vector2f(sf::Vector2f(mBackBorder->getPosition().x + 450, mBackBorder->getPosition().y + 10)));
-		mBox4->setFillColor(sf::Color(10, 10, 10, 230));
-		mBox4->setOutlineColor(sf::Color::White);
+		box4_ = new sf::RectangleShape(sf::Vector2f(100, 100));
+		box4_->setPosition(sf::Vector2f(sf::Vector2f(backboarder_->getPosition().x + 450, backboarder_->getPosition().y + 10)));
+		box4_->setFillColor(sf::Color(10, 10, 10, 230));
+		box4_->setOutlineColor(sf::Color::White);
 
-		mGUIElements.push_back(mHungerLevel);
-		mGUIElements.push_back(mThirstLevel);
-		mGUIElements.push_back(mHealthLevel);
-		mGUIElements.push_back(mRestLevel);
+		GUI_elements_.push_back(hunger_level_);
+		GUI_elements_.push_back(thirst_level_);
+		GUI_elements_.push_back(health_level_);
+		GUI_elements_.push_back(rest_level);
 
-		mGUIElements.push_back(mHungerMeter);
-		mGUIElements.push_back(mThirstMeter);
-		mGUIElements.push_back(mHealthMeter);
-		mGUIElements.push_back(mRestMeter);
+		GUI_elements_.push_back(hunger_meter_);
+		GUI_elements_.push_back(thrist_meter_);
+		GUI_elements_.push_back(health_meter_);
+		GUI_elements_.push_back(rest_meter);
 
-		mGUIElements.push_back(mBackBorder);
-		mGUIElements.push_back(mBox0);
-		mGUIElements.push_back(mBox1);
-		mGUIElements.push_back(mBox2);
-		mGUIElements.push_back(mBox3);
-		mGUIElements.push_back(mBox4);
+		GUI_elements_.push_back(backboarder_);
+		GUI_elements_.push_back(box0_);
+		GUI_elements_.push_back(box1_);
+		GUI_elements_.push_back(box2_);
+		GUI_elements_.push_back(box3_);
+		GUI_elements_.push_back(box4_);
 	}
 
 	void input(sf::Event* event) {
 		if (event->type == sf::Event::KeyPressed) {
 			switch (event->key.code) {
 			case sf::Keyboard::Num1:
-				mBoxSelected = 0;
+				box_selected_ = 0;
 				break;
 			case sf::Keyboard::Num2:
-				mBoxSelected = 1;
+				box_selected_ = 1;
 				break;
 			case sf::Keyboard::Num3:
-				mBoxSelected = 2;
+				box_selected_ = 2;
 				break;
 			case sf::Keyboard::Num4:
-				mBoxSelected = 3;
+				box_selected_ = 3;
 				break;
 			case sf::Keyboard::Num5:
-				mBoxSelected = 4;
+				box_selected_ = 4;
 				break;
 			case sf::Keyboard::Escape:
-				mWindow->close();
+				window_->close();
 				break;
 			}
 		}
 	}
 
 	void update(float deltaTime) {
-		mHungerLevel->setPosition(mHungerMeter->getPosition().x + 2, mHungerMeter->getPosition().y + 2);
-		mThirstLevel->setPosition(mThirstMeter->getPosition().x + 2, mThirstMeter->getPosition().y + 2);
-		mHealthLevel->setPosition(mHealthMeter->getPosition().x + 2, mHealthMeter->getPosition().y + 2);
-		mRestLevel->setPosition(mRestMeter->getPosition().x + 2, mRestMeter->getPosition().y + 2);
+		hunger_level_->setPosition(hunger_meter_->getPosition().x + 2, hunger_meter_->getPosition().y + 2);
+		thirst_level_->setPosition(thrist_meter_->getPosition().x + 2, thrist_meter_->getPosition().y + 2);
+		health_level_->setPosition(health_meter_->getPosition().x + 2, health_meter_->getPosition().y + 2);
+		rest_level->setPosition(rest_meter->getPosition().x + 2, rest_meter->getPosition().y + 2);
 
-		mHungerMeter->setPosition(sf::Vector2f(mPlayer->getPosition().x + 330.0f, mPlayer->getPosition().y - 445.0f));
-		mThirstMeter->setPosition(sf::Vector2f(mPlayer->getPosition().x + 405.0f, mPlayer->getPosition().y - 445.0f));
-		mHealthMeter->setPosition(sf::Vector2f(mPlayer->getPosition().x + 480.0f, mPlayer->getPosition().y - 445.0f));
-		mRestMeter->setPosition(sf::Vector2f(mPlayer->getPosition().x + 555.0f, mPlayer->getPosition().y - 445.0f));
+		hunger_meter_->setPosition(sf::Vector2f(player_->getPosition().x + 330.0f, player_->getPosition().y - 445.0f));
+		thrist_meter_->setPosition(sf::Vector2f(player_->getPosition().x + 405.0f, player_->getPosition().y - 445.0f));
+		health_meter_->setPosition(sf::Vector2f(player_->getPosition().x + 480.0f, player_->getPosition().y - 445.0f));
+		rest_meter->setPosition(sf::Vector2f(player_->getPosition().x + 555.0f, player_->getPosition().y - 445.0f));
 
-		mBackBorder->setPosition(sf::Vector2f(mPlayer->getPosition().x - 280.0f, mPlayer->getPosition().y + 355.0f));
-		mBox0->setPosition(sf::Vector2f(sf::Vector2f(mBackBorder->getPosition().x + 10, mBackBorder->getPosition().y + 10)));
-		mBox1->setPosition(sf::Vector2f(sf::Vector2f(mBackBorder->getPosition().x + 120, mBackBorder->getPosition().y + 10)));
-		mBox2->setPosition(sf::Vector2f(sf::Vector2f(mBackBorder->getPosition().x + 230, mBackBorder->getPosition().y + 10)));
-		mBox3->setPosition(sf::Vector2f(sf::Vector2f(mBackBorder->getPosition().x + 340, mBackBorder->getPosition().y + 10)));
-		mBox4->setPosition(sf::Vector2f(sf::Vector2f(mBackBorder->getPosition().x + 450, mBackBorder->getPosition().y + 10)));
+		backboarder_->setPosition(sf::Vector2f(player_->getPosition().x - 280.0f, player_->getPosition().y + 355.0f));
+		box0_->setPosition(sf::Vector2f(sf::Vector2f(backboarder_->getPosition().x + 10, backboarder_->getPosition().y + 10)));
+		box1_->setPosition(sf::Vector2f(sf::Vector2f(backboarder_->getPosition().x + 120, backboarder_->getPosition().y + 10)));
+		box2_->setPosition(sf::Vector2f(sf::Vector2f(backboarder_->getPosition().x + 230, backboarder_->getPosition().y + 10)));
+		box3_->setPosition(sf::Vector2f(sf::Vector2f(backboarder_->getPosition().x + 340, backboarder_->getPosition().y + 10)));
+		box4_->setPosition(sf::Vector2f(sf::Vector2f(backboarder_->getPosition().x + 450, backboarder_->getPosition().y + 10)));
 
-		if (mPlayer->getVitals().hunger < 50.0f)
-			mHungerLevel->setFillColor(mWarning);
-		if (mPlayer->getVitals().hunger < 10.0f)
-			mHungerLevel->setFillColor(mDanger);
-		if (mPlayer->getVitals().hunger < 0.0f)
-			mHungerLevel->setFillColor(mFatal);
+		if (player_->getVitals().hunger < 50.0f)
+			hunger_level_->setFillColor(warning_);
+		if (player_->getVitals().hunger < 10.0f)
+			hunger_level_->setFillColor(danger_);
+		if (player_->getVitals().hunger < 0.0f)
+			hunger_level_->setFillColor(fatal_);
 
-		if (mPlayer->getVitals().thirst < 50.0f)
-			mHungerLevel->setFillColor(mWarning);
-		if (mPlayer->getVitals().thirst < 10.0f)
-			mHungerLevel->setFillColor(mDanger);
-		if (mPlayer->getVitals().thirst < 0.0f)
-			mHungerLevel->setFillColor(mFatal);
+		if (player_->getVitals().thirst < 50.0f)
+			hunger_level_->setFillColor(warning_);
+		if (player_->getVitals().thirst < 10.0f)
+			hunger_level_->setFillColor(danger_);
+		if (player_->getVitals().thirst < 0.0f)
+			hunger_level_->setFillColor(fatal_);
 
-		if (mPlayer->getVitals().health < 50.0f)
-			mHungerLevel->setFillColor(mWarning);
-		if (mPlayer->getVitals().health < 10.0f)
-			mHungerLevel->setFillColor(mDanger);
-		if (mPlayer->getVitals().health < 0.0f)
-			mHungerLevel->setFillColor(mFatal);
+		if (player_->getVitals().health < 50.0f)
+			hunger_level_->setFillColor(warning_);
+		if (player_->getVitals().health < 10.0f)
+			hunger_level_->setFillColor(danger_);
+		if (player_->getVitals().health < 0.0f)
+			hunger_level_->setFillColor(fatal_);
 
-		if (mPlayer->getVitals().rest < 50.0f)
-			mHungerLevel->setFillColor(mWarning);
-		if (mPlayer->getVitals().rest < 10.0f)
-			mHungerLevel->setFillColor(mDanger);
-		if (mPlayer->getVitals().rest < 0.0f)
-			mHungerLevel->setFillColor(mFatal);
+		if (player_->getVitals().rest < 50.0f)
+			hunger_level_->setFillColor(warning_);
+		if (player_->getVitals().rest < 10.0f)
+			hunger_level_->setFillColor(danger_);
+		if (player_->getVitals().rest < 0.0f)
+			hunger_level_->setFillColor(fatal_);
 
-		switch (mBoxSelected) {
+		switch (box_selected_) {
 		case 0:
-			mBox0->setOutlineThickness(1.0f);
-			mBox1->setOutlineThickness(0.0f);
-			mBox2->setOutlineThickness(0.0f);
-			mBox3->setOutlineThickness(0.0f);
-			mBox4->setOutlineThickness(0.0f);
+			box0_->setOutlineThickness(1.0f);
+			box1_->setOutlineThickness(0.0f);
+			box2_->setOutlineThickness(0.0f);
+			box3_->setOutlineThickness(0.0f);
+			box4_->setOutlineThickness(0.0f);
 			break;
 		case 1:
-			mBox0->setOutlineThickness(0.0f);
-			mBox1->setOutlineThickness(1.0f);
-			mBox2->setOutlineThickness(0.0f);
-			mBox3->setOutlineThickness(0.0f);
-			mBox4->setOutlineThickness(0.0f);
+			box0_->setOutlineThickness(0.0f);
+			box1_->setOutlineThickness(1.0f);
+			box2_->setOutlineThickness(0.0f);
+			box3_->setOutlineThickness(0.0f);
+			box4_->setOutlineThickness(0.0f);
 			break;
 		case 2:
-			mBox0->setOutlineThickness(0.0f);
-			mBox1->setOutlineThickness(0.0f);
-			mBox2->setOutlineThickness(1.0f);
-			mBox3->setOutlineThickness(0.0f);
-			mBox4->setOutlineThickness(0.0f);
+			box0_->setOutlineThickness(0.0f);
+			box1_->setOutlineThickness(0.0f);
+			box2_->setOutlineThickness(1.0f);
+			box3_->setOutlineThickness(0.0f);
+			box4_->setOutlineThickness(0.0f);
 			break;
 		case 3:
-			mBox0->setOutlineThickness(0.0f);
-			mBox1->setOutlineThickness(0.0f);
-			mBox2->setOutlineThickness(0.0f);
-			mBox3->setOutlineThickness(1.0f);
-			mBox4->setOutlineThickness(0.0f);
+			box0_->setOutlineThickness(0.0f);
+			box1_->setOutlineThickness(0.0f);
+			box2_->setOutlineThickness(0.0f);
+			box3_->setOutlineThickness(1.0f);
+			box4_->setOutlineThickness(0.0f);
 			break;
 		case 4:
-			mBox0->setOutlineThickness(0.0f);
-			mBox1->setOutlineThickness(0.0f);
-			mBox2->setOutlineThickness(0.0f);
-			mBox3->setOutlineThickness(0.0f);
-			mBox4->setOutlineThickness(1.0f);
+			box0_->setOutlineThickness(0.0f);
+			box1_->setOutlineThickness(0.0f);
+			box2_->setOutlineThickness(0.0f);
+			box3_->setOutlineThickness(0.0f);
+			box4_->setOutlineThickness(1.0f);
 			break;
 		}
 	}
 private:
-	sf::Sprite *mHungerMeter, *mThirstMeter, *mHealthMeter, *mRestMeter;
-	sf::RectangleShape *mBackBorder, *mBox0, *mBox1, *mBox2, *mBox3, *mBox4;
-	sf::CircleShape *mHungerLevel, *mThirstLevel, *mHealthLevel, *mRestLevel;
-	sf::Color mHealthy, mWarning, mDanger, mFatal;
-	int mBoxSelected;
-	Player* mPlayer;
+	sf::Sprite *hunger_meter_, *thrist_meter_, *health_meter_, *rest_meter;
+	sf::RectangleShape *backboarder_, *box0_, *box1_, *box2_, *box3_, *box4_;
+	sf::CircleShape *hunger_level_, *thirst_level_, *health_level_, *rest_level;
+	sf::Color healthy_, warning_, danger_, fatal_;
+	int box_selected_;
+	Player* player_;
 };
 
 #endif
